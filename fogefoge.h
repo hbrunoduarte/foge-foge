fb4c2241d624bb8bd49d728b70d564df53d4af36
Bruno Duarte <hbrunoduarte@gmail.com>
2024-08-06 13:01:15 -0300

    projeto inicial

---
+++ fogefoge.h
@@ -0,0 +1,18 @@
+#ifndef _FOGE_FOGE_
+#define _FOGE_FOGE_
+
+#define CIMA 'w'
+#define BAIXO 's'
+#define DIREITA 'd'
+#define ESQUERDA 'a'
+#define BOMBA 'b'
+
+int acabou();
+void move(char direcao);
+
+int ehdirecao(char direcao);
+void fantasmas();
+
+int praondefantasmavai(int xatual, int yatual, int* xdestino, int* ydestino);
+
+#endif
---
+++ mapa.h
@@ -0,0 +1,46 @@
+#ifndef _MAPA_H_
+#define _MAPA_H_
+
+#define HEROI '@'
+#define VAZIO '.'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define FANTASMA 'F'
+#define PILULA 'P'
+
+struct mapa {
+	char** matriz;
+	int linhas;
+	int colunas; 
+};
+
+typedef struct mapa MAPA;
+
+void alocamapa(MAPA* m);
+void lemapa(MAPA* m);
+void liberamapa(MAPA* m);
+void imprimemapa(MAPA* m);
+
+struct posicao {
+	int x;
+	int y;
+};
+
+typedef struct posicao POSICAO;
+
+int encontramapa(MAPA* m, POSICAO* p, char c);
+
+int ehvalida(MAPA* m, int x, int y);
+int ehvazia(MAPA*, int x, int y);
+
+void andanomapa(MAPA* m, int xorigem, int yorigem,
+	int xdestino, int ydestino);
+
+void copiamapa(MAPA* destino, MAPA* origem);
+int podeandar(MAPA* m, char personagem, int x, int y);
+int ehparede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
+void explodepilula();
+void explodepilula2(int x, int y, int somax, int somay, int qtd);
+
+#endif
